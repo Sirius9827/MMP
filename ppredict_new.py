7003e0c91a08863bffb231889962a2fb977eb5be
Sirius9827 <161190118@smail.nju.edu.cn>
2024-07-29 15:10:20 +0000

    modified_on_three_datasets

--- ppredict_new.py
+++ ppredict_new.py
@@ -1,16 +1,16 @@
 import random
 
 import numpy as np
-<<<<<<< HEAD
-from utils import DatasetLoader, ModelTuner, save_results
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from utils import DatasetLoader, MMPmodel, save_results
 import argparse
 import os
 from sklearn.metrics import root_mean_squared_error, roc_auc_score
-=======
-from utils import DatasetLoader, ModelTuner, MMPmodel
-import argparse
-import os
->>>>>>> origin/master
+from sklearn.model_selection import ParameterGrid, train_test_split
+from sklearn.metrics import roc_auc_score, mean_squared_error
+import numpy as np
 
 # Define command-line arguments
 def parse_args():
@@ -32,29 +32,127 @@ if __name__ == '__main__':
     task_binary = dataloader.task_binary(Y)
     print("Task is binary: ", task_binary)
 
-    seeds = [42, 1234, 7]
-    results = []
-    for seed in seeds:
-        # tune hyperparameters for the model and dataset
-        X_train, X_val, X_test, y_train, y_val, y_test = dataloader.split(X, Y, seed)
-        tuner = ModelTuner(args.model, X_train, y_train, task_binary, seed, args.n_jobs)
-        best_model, best_param = tuner.tune_para()
-        # save parameters for the best model
-        predictions_val = best_model.predict(X_test)
+    # Assuming self.model is 'SVM', 'XGB', or 'RF'
+    if args.model == 'SVM':
+        model = SVC()
+        param_grid = {
+            'C': [0.1, 1, 10, 100],
+            'gamma': [0.2, 0.1, 0.01, 0.001],
+            'kernel': ['rbf'],
+        }
+    elif args.model == 'XGB':
+        model = XGBClassifier()
+        param_grid = {
+            'n_estimators': [50, 100, 200, 300, 400, 500, 1000],
+            'max_depth': [3, 5, 7, 9, 10],
+            'learning_rate': [0.01, 0.1, 0.2],
+            'subsample': [0.7, 0.8, 0.9, 1.0],
+            'colsample_bytree': [0.7, 0.8, 0.9, 1.0],
+            'gamma': [0, 0.1, 0.2],
+            'min_child_weight': [1, 3, 6],
+        }
+    elif args.model == 'RF':
+        model = RandomForestClassifier()
+        param_grid = {
+            'n_estimators': [50, 100, 200, 300, 400, 500, 1000],
+            'max_features': ['sqrt', 'log2', 0.7, 0.8, 0.9],
+            'max_depth': [3, 6, 8, 10, 12],
+            'min_samples_leaf': [1, 3, 5, 10, 20, 50],
+            'min_impurity_decrease': [0.0, 0.01],
+        }
         if task_binary:
-            auc = roc_auc_score(y_test, predictions_val)
+            param_grid['criterion'] = ['gini', 'entropy']
+        else:
+            param_grid['criterion'] = ['squared_error']
+
+    param_combinations = list(ParameterGrid(param_grid))
+    best_score = -np.inf if task_binary else np.inf
+    best_params = None
+    best_std = None
+
+    # Split the data into 3 random splits
+    seeds = [42, 1234, 7]
+    # splits = [train_test_split(X, Y, test_size=0.1, random_state=seed) for seed in seeds]
+    splits = []
+
+    for params in param_combinations:
+        scores = []
+        for seed in seeds:
+            X_train, X_val, y_train, y_val = train_test_split(X, Y, test_size=0.2, random_state=seed)
+            X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.5, random_state=seed)
+            splits.append([X_test, y_test])
+            mmp = MMPmodel(model, X_train, y_train, task_binary, seed)
+            model = mmp.get_model(args.model, seed)
+
+            model.set_params(**params)
+            model.fit(X_train, y_train)
+            predictions_val = model.predict(X_val)
+            
+            if task_binary:
+                auc = roc_auc_score(y_val, predictions_val)
+                scores.append(auc)
+            else:
+                rmse = root_mean_squared_error(y_val, predictions_val)
+                scores.append(rmse)
+        
+        avg_score = np.mean(scores)
+        std_score = np.std(scores)
+        
+        if (task_binary and avg_score > best_score) or (not task_binary and avg_score < best_score):
+            best_score = avg_score
+            best_params = params
+            best_std = std_score
+    print("Best Parameters:", best_params)
+    print("Best Average Score on validatioin:", best_score)
+
+    # Fit the model with the best parameters on the full training data and predict on the test set
+    results = []
+    for X_test, y_test in splits:
+        model.set_params(**best_params)
+        model.fit(X_test, y_test)
+        y_pred = model.predict(X_test)
+        if task_binary:
+            auc = roc_auc_score(y_val, predictions_val)
             results.append(auc)
         else:
-            rmse = root_mean_squared_error(y_test, predictions_val)
+            rmse = root_mean_squared_error(y_val, predictions_val)
             results.append(rmse)
+    mean_score = np.mean(results)
+    std_score = np.std(results)
+    
     if task_binary:
-        auc_mean = np.mean(results)
-        auc_std = np.std(results)
-        rsl_save = {'AUC': {'mean': auc_mean, 'std': auc_std}}
+        rsl_save = {'AUC': {'mean': mean_score, 'std': std_score}}
     else:
-        rmse_mean = np.mean(results)
-        rmse_std = np.std(results)
-        rsl_save = {'RMSE': {'mean': rmse_mean, 'std': rmse_std}}
+        rsl_save = {'RMSE': {'mean': mean_score, 'std': std_score}}
+
+
+    print("Best Parameters:", best_params)
+    print("Best Average Score on test:", mean_score)
+    print("Standard Deviation of Score:", std_score)
+
+    # seeds = [42, 1234, 7]
+    # results = []
+    # for seed in seeds:
+    #     # tune hyperparameters for the model and dataset
+    #     X_train, X_val, X_test, y_train, y_val, y_test = dataloader.split(X, Y, seed)
+    #     tuner = MMPmodel(args.model, X_train, y_train, X_val, y_val, task_binary, seed, args.n_jobs)
+    #     best_model, best_param = tuner.tune_para()
+    #     # save parameters for the best model
+    #     predictions_val = best_model.predict(X_test)
+    #     if task_binary:
+    #         auc = roc_auc_score(y_test, predictions_val)
+    #         results.append(auc)
+    #     else:
+    #         rmse = root_mean_squared_error(y_test, predictions_val)
+    #         results.append(rmse)
+    # if task_binary:
+    #     auc_mean = np.mean(results)
+    #     auc_std = np.std(results)
+    #     rsl_save = {'AUC': {'mean': auc_mean, 'std': auc_std}}
+    # else:
+    #     rmse_mean = np.mean(results)
+    #     rmse_std = np.std(results)
+    #     rsl_save = {'RMSE': {'mean': rmse_mean, 'std': rmse_std}}
     save_results(rsl_save, args.dataset_name, args.model, args.output_dir)
 
 
